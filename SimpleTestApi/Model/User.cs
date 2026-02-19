6ae0277f1f26c35d05321d64ca4a06b985130384
Sonam-tech98 <sonamtiwari7620.st@gmail.com>
2026-02-19 16:50:53 +0530

    Update

---
+++ SimpleTestApi/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleTestApi.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
