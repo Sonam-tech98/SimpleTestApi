9c021f7779c8629bea3e424fb2054588b15c7f49
Sonam-tech98 <sonamtiwari7620.st@gmail.com>
2026-02-19 13:19:30 +0530

    Update

--- SimpleTestApi/Program.cs
+++ SimpleTestApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
-        Title = "My API",
+        Title = "SimpleTestApi",
         Version = "v1"
     });
 });
@@ -25,7 +25,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleTestApi");
 });
 //app.UseHttpsRedirection();
 
